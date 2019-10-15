36a63a6976214c9332d5ae9336f488b9a114137f
superfroggman <49985901+superfroggman@users.noreply.github.com>
2019-10-15 11:45:24 +0200

    removed multithreading, didnt work

--- cloud-remover.py
+++ cloud-remover.py
@@ -1,11 +1,12 @@
 import sys
 import imageio
-import threading
 
 vid = imageio.get_reader(sys.argv[1],  'ffmpeg')
 base_img = vid.get_data(0)
-
-def doStuff(*img):
+for l in range(vid.count_frames()):
+    img = vid.get_data(l)
+    print("frame")
+    print(l)
     for i in range(len(img)):
         for j in range(len(img[1])):
             r, g, b = img[i][j]
@@ -14,12 +15,6 @@ def doStuff(*img):
             totalOld = (int(r2)+int(g2)+int(b2))/3
             if(totalNew < totalOld):
                 base_img[i][j] = img[i][j]
-
-
-for l in range(0, vid.count_frames()):
-    img = vid.get_data(l)
-    t1 = threading.Thread(target=doStuff, args=(img))
-    t1.start()
 
 print ("DONE! Ignore errors after this.")
 imageio.imwrite(sys.argv[2], base_img)
